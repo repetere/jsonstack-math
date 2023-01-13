83da35ff68366b69e7e01e9a501b0583016a15f9
Yaw Joseph Etse <yaw.etse@gmail.com>
2023-01-12 20:37:45 -0800

    feat: paramaterized vector solutions

--- src/System.test.ts
+++ src/System.test.ts
@@ -32,17 +32,38 @@ describe('System',()=>{
     });
   });
   describe('solve',()=>{
-    it('solve the system',async ()=>{
+    // it('solves a consistent and unique solution system',async ()=>{
+    //   const a = [
+    //     [1, -2, 1, 0],
+    //     [0, 2, -8, 8],
+    //     [5, 0, -5, 10],
+    //   ]
+    //   const A = new System(a);
+    //   const solution = await A.solve();
+
+    //   expect(solution.solutions.x_1).toBe(1);
+    //   expect(solution.solutions.x_2).toBe(0);
+    //   expect(solution.solutions.x_3).toBe(-1);
+    //   expect(solution.unique).toBe(true);
+    //   expect(solution.consistent).toBe(true);
+    //   expect(solution.pivots).toHaveLength(3);
+    //   console.log({solution})
+    // });
+    it('solves a consistent and non unique solution system',async ()=>{
       const a = [
-        [1, -2, 1, 0],
-        [0, 2, -8, 8],
-        [5, 0, -5, 10],
+        [1, 4, -5, 0],
+        [2, -1, 8, 9],
       ]
       const A = new System(a);
       const solution = await A.solve();
-      // const m = new System([[1,2,3],[4,5,6]]);
-      // expect(m.solve()).toBeInstanceOf(Vector);
-      // expect(m.solve().get()).toEqual([3,6]);
+
+      // expect(solution.solutions.x_1).toBe(1);
+      // expect(solution.solutions.x_2).toBe(0);
+      // expect(solution.solutions.x_3).toBe(-1);
+      // expect(solution.unique).toBe(true);
+      // expect(solution.consistent).toBe(true);
+      // expect(solution.pivots).toHaveLength(3);
+      console.log({solution})
     });
   });
 });
--- src/System.ts
+++ src/System.ts
@@ -28,6 +28,48 @@ export class System{
     }
     return labelNames;
   }
+  parameterizeSolution({
+    freeVariableColumnIndexes,
+    basicVariableColumnIndexes,
+    coefficients,
+    augmentedColumn,
+    labelNames,
+  }:any){
+    const vectorSolution:{[key: string]:number[]}={};
+    const solutionCoffientMatrix:number[][]=[];
+    const solutionAugmentedVector:number[]=labelNames.map((labelName:string,rowIndex:number)=>{
+      return (freeVariableColumnIndexes.includes(rowIndex))
+        ? 0
+        : augmentedColumn[rowIndex];
+    });
+    const emptyRow = coefficients[0].map(()=>0);
+    labelNames.forEach((labelName:string,rowIndex:number)=>{
+      let solutionCoefficientRow:number[] = [].concat(emptyRow);
+      if(freeVariableColumnIndexes.includes(rowIndex)){
+        solutionCoefficientRow[rowIndex] = 1;
+      } else if(rowIndex<coefficients.length){
+        solutionCoefficientRow = solutionCoefficientRow.map((value,columnIndex)=>{
+          let coefficientValue = coefficients[rowIndex][columnIndex];
+          if(basicVariableColumnIndexes.includes(columnIndex) && coefficientValue===1){
+            coefficientValue = 0;
+          } else coefficientValue =(coefficientValue!==0)? -1*coefficientValue:0;
+          return coefficientValue;
+        })
+      }
+      solutionCoffientMatrix.push(solutionCoefficientRow);
+    });
+    const A = new Matrix(solutionCoffientMatrix);
+    console.log({solutionCoffientMatrix,solutionAugmentedVector})
+    vectorSolution.vector = solutionAugmentedVector;
+    solutionCoffientMatrix[0].forEach((columnValue,columnIndex)=>{
+      const solutionVector = A.column(columnIndex).get();
+      if(solutionVector.every((value)=>value!==0)){
+        vectorSolution[labelNames[columnIndex]] = solutionVector;
+      }
+    });
+    console.log({vectorSolution})
+    return vectorSolution;
+  }
   async solve(){
     const A = this.system.rref();
     const B = new System(A);
@@ -44,11 +86,38 @@ export class System{
       }
     });
     if(this.consistent===undefined) this.consistent = true;
-    this.unique = mainDiagonal.get().every((value)=>value===1) && mainDiagonal.get().length === augmentedColumn.get().length;
+    this.unique = mainDiagonal.get().every((value)=>value===1) && mainDiagonal.get().length === (coefficients.row(0) as number[]).length;
     const pivots = coefficients.pivots;
     const basicVariableColumnIndexes = pivots.map((pivot)=>pivot[1]);
-    const freeVariableColumnIndexes = (coefficients.row(0) as number[]).filter((rowValue,columnIndex)=>basicVariableColumnIndexes.includes(columnIndex)===false).map((rowValue,columnIndex)=>columnIndex);
+    const freeVariableColumnIndexes = (coefficients.row(0) as number[]).reduce((result:number[],rowValue:number,columnIndex:number)=>{
+      if(basicVariableColumnIndexes.includes(columnIndex)===false){
+        result.push(columnIndex);
+      }
+      return result;
+    },[])
+    const uniqueSolutions= basicVariableColumnIndexes.reduce((solutions,columnIndex)=>{
+      const labelName = this.labelNames[columnIndex];
+      const coffientRowSum = coefficients.elements.slice(columnIndex,1).sum().arraySync();
+      // console.log({coffientRowSum,columnIndex,labelName});
+      if(coffientRowSum===1){
+        const solutionValue = augmentedColumn.get()[columnIndex];
+        solutions[labelName] = solutionValue;
+      }
+      return solutions;
+    },{} as {[key:string]:number});
 
+
+    
+    const parameterizedSolutions = (this.unique)
+      ?{}
+      :this.parameterizeSolution({
+        freeVariableColumnIndexes,
+        basicVariableColumnIndexes,
+        coefficients: coefficients.get(),
+        augmentedColumn: augmentedColumn.get(),
+        labelNames: this.labelNames,
+      });
+    const solutions = {...uniqueSolutions,...parameterizedSolutions};
     console.log(A.get(),{
       coefficients: coefficients.get(),
       augmentedColumn: augmentedColumn.get(),
@@ -60,7 +129,19 @@ export class System{
       basicVariableColumnIndexes,
       freeVariableColumnIndexes,
       labelNames: this.labelNames,
+      solutions
     });
     this.solved = true;
+
+    return {
+      coefficients,
+      augmentedColumn,
+      unique: this.unique,
+      consistent: this.consistent,
+      pivots,
+      basicVariableColumnIndexes,
+      freeVariableColumnIndexes,
+      labelNames: this.labelNames,
+    }
   }
 }
